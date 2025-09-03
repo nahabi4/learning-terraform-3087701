ce57ad755b976de448b2ad1edd01e5215de23b50
nahabi4 <nahabi4@gmail.com>
2025-09-03 19:12:21 +0200

    load balancer

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_security-group.security_group_id]
@@ -57,4 +57,40 @@ module "blog_security-group" {
 
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "blog-alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = [
+    name_prefix = "blog-"
+    backend_protocol = "HTTP"
+    backend_port = 80
+    target_type = "instance"
+
+    targets = {
+      my_target = {
+        target_id = aws_instance.blog.id
+        port = 80 
+      }
+    }
+
+  ]
+
+  http_tcp_listeners = [
+    {
+      port = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
 }
