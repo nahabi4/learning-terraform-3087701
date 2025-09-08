69dfa999036ecba83d60611924b43ce34d6e9b02
nahabi4 <nahabi4@gmail.com>
2025-09-08 18:49:12 +0200

    ad

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] 
 }
 
 data "aws_vpc" "default" {
@@ -21,11 +21,11 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.netwrok_prefix}.0.0/16"
 
   azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.netwrok_prefix}.101.0/24", "${var.environment.netwrok_prefix}.102.0/24", "${var.environment.netwrok_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
@@ -39,9 +39,9 @@ module "autoscaling" {
   version = "9.0.1"
   # insert the 1 required variable here
 
-  name = "blog"
-  min_size = 1
-  max_size = 1
+  name = "${var.environment.netwrok_prefix}-blog"
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
@@ -59,7 +59,7 @@ resource "aws_autoscaling_attachment" "asg_attachment" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog-sg"
+  name = "${var.environment.netwrok_prefix}-blog-sg"
 
   vpc_id = module.blog_vpc.vpc_id
 
@@ -74,14 +74,14 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name    = "blog-alb"
+  name    = "${var.environment.netwrok_prefix}-blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
-      name_prefix = "blog-"
+      name_prefix = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port = 80
       target_type = "instance"
@@ -99,7 +99,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.netwrok_prefix
   }
 }
 
