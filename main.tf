3a479b401dd6431ad0f6f00009a2490a81d4b127
nahabi4 <nahabi4@gmail.com>
2025-09-08 18:15:32 +0200

    cc

--- main.tf
+++ main.tf
@@ -33,16 +33,22 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+  # insert the 1 required variable here
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpx_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]  
+
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
 module "blog_sg" {
@@ -59,7 +65,7 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-module "blog-alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
@@ -91,3 +97,5 @@ module "blog-alb" {
     Environment = "dev"
   }
 }
+
+
